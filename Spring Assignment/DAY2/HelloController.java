39a4eef2036ac87aad7ef005e280b5513ba8a752
ThrupthiNS <92461041+ThrupthiNS@users.noreply.github.com>
2021-11-22 08:46:25 +0530

    Update HelloController.java

--- Spring Assignment/DAY2/HelloController.java
+++ Spring Assignment/DAY2/HelloController.java
@@ -20,12 +20,12 @@ import org.springframework.web.servlet.ModelAndView;
 //        return "hello";
 //    }
 	
-	@RequestMapping("/NewFile")
+	@RequestMapping("/Another")
     public ModelAndView getlogin(@RequestParam("username") String username,@RequestParam("password") String password, Model model) {             
 		ModelAndView mav=new ModelAndView("First");
 		model.addAttribute("username",username);
 		model.addAttribute("password",password);
-		int f11=0;
+		int f=0;
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -35,7 +35,7 @@ import org.springframework.web.servlet.ModelAndView;
 			p1.setString(2, password);
 			ResultSet rs=p1.executeQuery();
 			if(rs.next()) {
-				f11=1;
+				f=1;
 				return mav;
 			}
 		}
